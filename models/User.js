5f3ef4ebf5f9a495024a9b5f7f81f72313153df7
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-08-17 18:32:46 -0400

    started user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true, 
+        trim: true, 
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        validate: [validateEmail, 'Please use a valid email address'], 
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please use a valid email address']
+    },
+    thoughts: []
+})
