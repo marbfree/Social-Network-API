221d107f5356fadde21d4a60cc2dc9c5fd587074
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-08-18 09:53:37 -0400

    completed userController file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,65 @@
 const User = require('../models/User');
 
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .populate('thoughts', 'friends')
+                .select('-__v')
+
+            if (!user) {
+                return res.status(400).json({ message: 'No user with that ID' })
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                return res.status(400).json({ message: 'No user with this id!'})
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err)
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(400).json({ message: 'No user with that ID' });
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            res.json({ message: 'User and associated thoughts deleted!' })
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
